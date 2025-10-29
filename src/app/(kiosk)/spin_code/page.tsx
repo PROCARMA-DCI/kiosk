6141044689bfa914ba3bc999a29ee77ec448afc0
sbshamail123 <sbshamail123@gmail.com>
2025-10-29 17:11:23 +0500

    update

--- src/app/(kiosk)/spin_code/page.tsx
+++ src/app/(kiosk)/spin_code/page.tsx
@@ -102,7 +102,7 @@ function LoyaltySpinInner() {
         </h1>
 
         <div className="spin-code-gradient  w-[523px] min-h-[504px]  rounded-[85px] shadow-2xl flex justify-center items-center ">
-          <Card className="  w-[463.71px] h-[455.85px] overflow-hidden rounded-[60px] flex items-center justify-center bg-white dark:bg-gray-900 border-none">
+          <Card className=" w-[463.71px] h-[455.85px] overflow-hidden rounded-[60px] flex items-center justify-center bg-white dark:bg-gray-900 border-none">
             <CardContent className="flex flex-col items-center justify-center p-8 space-y-8">
               <div
                 className="text-2xl font-semibold  bg-clip-text text-transparent"
