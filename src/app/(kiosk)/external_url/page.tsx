4716e72fc13dd66bdcf22962134a8f4a4a1dcabd
sbshamail123 <sbshamail123@gmail.com>
2025-11-27 12:48:55 +0500

    update

--- src/app/(kiosk)/external_url/page.tsx
+++ src/app/(kiosk)/external_url/page.tsx
@@ -1,9 +1,11 @@
+import BackButton from "@/common/BackButton";
 import IframeUrl from "@/common/IframeUrl";
 import { Suspense } from "react";
 
 export default function Page() {
   return (
     <Suspense fallback={<div className="p-10 text-center">Loading...</div>}>
+      <BackButton backRoute="/" />
       <IframeUrl />
     </Suspense>
   );
